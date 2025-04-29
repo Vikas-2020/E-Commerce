456945db8d3da2af475fc72419639a024c7ce71c
Vikas-2020 <vikaskr.shiv@gmail.com>
2025-04-29 08:50:46 +0530

    created env variable

--- src/firebase.js
+++ src/firebase.js
@@ -3,28 +3,28 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 import {
-    getAuth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut,
-    sendPasswordResetEmail,
-    updatePassword,
-  } from "firebase/auth"
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  sendPasswordResetEmail,
+  updatePassword,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDqISftnvhG2QUuimVRax8pezR56SY4OKY",
-  authDomain: "e-commerce-99da8.firebaseapp.com",
-  databaseURL: "https://e-commerce-99da8-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "e-commerce-99da8",
-  storageBucket: "e-commerce-99da8.firebasestorage.app",
-  messagingSenderId: "735630252133",
-  appId: "1:735630252133:web:ba1e567dd759c7d40e0e8c",
-  measurementId: "G-7Y49ECYRMK"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  databaseURL: import.meta.env.VITE_databaseURL,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
 };
 
 // Initialize Firebase
@@ -32,11 +32,11 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 export {
-    auth,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    onAuthStateChanged,
-    signOut,
-    sendPasswordResetEmail,
-    updatePassword,
-  };
+  auth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  sendPasswordResetEmail,
+  updatePassword,
+};
